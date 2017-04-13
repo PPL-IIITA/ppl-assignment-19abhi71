a9ef16aa3fbe80a55679477a5f59cfe08a008284
Abhishek Kumar <iit2015077@iiita.ac.in>
2017-04-14 00:11:57 +0530

    Update main.py

--- Q_1/main.py
+++ Q_1/main.py
@@ -1,5 +1,11 @@
-from boy import Boy
-from girl import Girl
+try:
+	from boy import Boy
+	from girl import Girl
+	from gift import Gifts
+	from couples import Couple
+	from magic import awesome
+except ImportError:
+	print('There is error in importing some files')
 from datetime import datetime
 import random
 import csv
@@ -18,10 +24,13 @@ boyslist=[]
 girlslist=[]
 
 #generating a list named B of boy objects and boyslist with respective values of object for writing to csv
-for i in range(19):
-	b=Boy(boys[i],random.randint(0,100),random.randint(0,100),random.randint(0,5000),random.randint(0,50),random.choice(typ1))
-	boyslist.append([b.name,b.attractiveness,b.intelligence,b.budget,b.minattreq,b.typ,b.status])
-	B.append(b)
+try:
+	for i in range(22):
+		b=Boy(boys[i],random.randint(10,100),random.randint(24,100),random.randint(50,5000),random.randint(6,50),random.choice(typ1))
+		boyslist.append([b.name,b.attractiveness,b.intelligence,b.budget,b.minattreq,b.typ,b.status])
+		B.append(b)
+except IndexError:
+	print('List out of bounds')
 
 #writing csv file
 with open("guys.csv","w") as h:
@@ -29,31 +38,44 @@ with open("guys.csv","w") as h:
 	writer.writerows(boyslist)
 
 #generating a list named G of girl objects and girlslist with respective values of object for writing to csv
-for i in range(13):
-	g=Girl(girls[i],random.randint(0,100),random.randint(0,100),random.randint(0,5000),random.choice(crit),random.choice(typ2))
-	girlslist.append([g.name,g.attractiveness,g.intelligence,g.maintbudget,g.criteria,g.typ,g.status])
-	G.append(g)
+try:
+	for i in range(13):
+		g=Girl(girls[i],random.randint(10,100),random.randint(26,100),random.randint(12,5000),random.choice(crit),random.choice(typ2))
+		girlslist.append([g.name,g.attractiveness,g.intelligence,g.maintbudget,g.criteria,g.typ,g.status])
+		G.append(g)
+except StopIteration:
+	print('List out of bounds')
 
 #writing csv file
 with open("girls.csv","w") as f:
 	writer=csv.writer(f,delimiter=',')
 	writer.writerows(girlslist)
 
-for boy in B:
-	for girl in G:
-		#if boy and girls are both ready to pair with each other and are already not commited
-		if boy.readytopair(girl) and girl.readytopair(boy) and boy.currStatus()=='S' and girl.currStatus()=='S':
-			#change their status
-			boy.changeStatus()
-			girl.changeStatus()
-			s1=boy.name+' is gonna date '+girl.name
-			commit.append(s1)
-			break
+try:
+	for boy in B:
+		for girl in G:
+			#if boy and girls are both ready to pair with each other and are already not commited
+			if boy.readytopair(girl) and girl.readytopair(boy) and boy.currStatus()=='S' and girl.currStatus()=='S':
+				#change their status
+				boy.changeStatus()
+				girl.changeStatus()
+						
+				c=Couple(boy.name,boy.typ,girl.typ,girl.name,boy.budget,girl.maintbudget,boy.attractiveness,girl.attractiveness,boy.intelligence,girl.intelligence)
+				couplesList.append(c)
+				s1=boy.name+' is gonna date '+girl.name
+				commit.append(s1)
+				break
+except IndentationError:
+	print('unexpected indentations')
 
 #taking note of all commitments
-file=open("log.txt","w")
-for item in commit:
-	file.write(str(datetime.now()))
-	file.write(" %s\n"%item)
-
+try:
+	file=open("log.txt","r")
+	for item in commit:
+		file.write(str(datetime.now()))
+		file.write(" %s\n"%item)
+except IOError:
+	print('Error can\'t write to file')
+finally:
+	file.close()
 
