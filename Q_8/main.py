9921971808ef103e8460ad7aa96f25d549b6a4f3
Abhishek Kumar <iit2015077@iiita.ac.in>
2017-04-14 00:51:22 +0530

    Update main.py

--- Q_8/main.py
+++ Q_8/main.py
@@ -1,7 +1,12 @@
-from boy import Boy
-from girl import Girl
-from gift import Gifts
-from couples import Couple
+try:
+	from boy import Boy
+	from girl import Girl
+	from gift import Gifts
+	from couples import Couple
+	from magic import awesome
+except ImportError:
+	print('There is error in importing some files')
+
 from basket import GiftBasket,GiftSelector1,GiftSelector2
 from datetime import datetime
 import operator
@@ -27,10 +32,13 @@ couplesList=[]
 #testing utility
 
 #generating a list named B of boy objects and boyslist with respective values of object for writing to csv
-for i in range(19):
-	b=Boy(boys[i],random.randint(10,100),random.randint(24,100),random.randint(1,20),random.randint(6,50),random.choice(typ1))
-	boyslist.append([b.name,b.attractiveness,b.intelligence,b.budget,b.minattreq,b.typ,b.status])
-	B.append(b)
+try:
+	for i in range(22):
+		b=Boy(boys[i],random.randint(10,100),random.randint(24,100),random.randint(50,5000),random.randint(6,50),random.choice(typ1))
+		boyslist.append([b.name,b.attractiveness,b.intelligence,b.budget,b.minattreq,b.typ,b.status])
+		B.append(b)
+except IndexError:
+	print('List out of bounds')
 
 #writing csv file
 
@@ -39,10 +47,13 @@ with open("guys.csv","w") as h:
 	writer.writerows(boyslist)
 
 #generating a list named G of girl objects and girlslist with respective values of object for writing to csv
-for i in range(13):
-	g=Girl(girls[i],random.randint(10,100),random.randint(26,100),random.randint(1,20),random.choice(crit),random.choice(typ2))
-	girlslist.append([g.name,g.attractiveness,g.intelligence,g.maintbudget,g.criteria,g.typ,g.status])
-	G.append(g)
+try:
+	for i in range(13):
+		g=Girl(girls[i],random.randint(10,100),random.randint(26,100),random.randint(12,5000),random.choice(crit),random.choice(typ2))
+		girlslist.append([g.name,g.attractiveness,g.intelligence,g.maintbudget,g.criteria,g.typ,g.status])
+		G.append(g)
+except StopIteration:
+	print('List out of bounds')
 
 #writing csv file
 with open("girls.csv","w") as f:
@@ -50,18 +61,24 @@ with open("girls.csv","w") as f:
 	writer.writerows(girlslist)
 
 def findDates():
-	for boy in B:
-		for girl in G:
-			#if boy and girls are both ready to pair with each other and are already not commited
-			if boy.readytopair(girl) and girl.readytopair(boy) and boy.currStatus()=='S' and girl.currStatus()=='S':
-				#change their status
-				boy.changeStatus()
-				girl.changeStatus()
-				c=Couple(boy.name,boy.typ,girl.typ,girl.name,boy.budget,girl.maintbudget,boy.attractiveness,girl.attractiveness,boy.intelligence,girl.intelligence,0)
-				couplesList.append(c)
-				s1=boy.name+' is gonna date '+girl.name
-				commit.append(s1)
-				break
+	try:
+		for boy in B:
+			for girl in G:
+				#if boy and girls are both ready to pair with each other and are already not commited
+				if boy.readytopair(girl) and girl.readytopair(boy) and boy.currStatus()=='S' and girl.currStatus()=='S':
+					#change their status
+					boy.changeStatus()
+					girl.changeStatus()
+						
+					c=Couple(boy.name,boy.typ,girl.typ,girl.name,boy.budget,girl.maintbudget,boy.attractiveness,girl.attractiveness,boy.intelligence,girl.intelligence)
+					couplesList.append(c)
+					s1=boy.name+' is gonna date '+girl.name
+					commit.append(s1)
+					break
+	except IndentationError:
+		print('unexpected indentations')
+
+
 findDates()
 
 
@@ -75,11 +92,15 @@ if choice==0:
 	commit.append(s1)
 	#taking note of all commitments and gift exchanges with time stamps3
 
-	file=open("log.txt","w")
-	for item in commit:
-		file.write(str(datetime.now()))
-		file.write(" %s\n"%item)
-	file.close()
+	try:
+		file=open("log.txt","r")
+		for item in commit:
+			file.write(str(datetime.now()))
+			file.write(" %s\n"%item)
+	except IOError:
+		print('Error can\'t write to file')
+	finally:
+		file.close()
 	g1=GiftSelector1(couplesList)
 	g1.assign()
 	g1.writ()
@@ -89,11 +110,15 @@ else:
 	commit.append(s1)
 	#taking note of all commitments and gift exchanges with time stamps3
 
-	file=open("log.txt","w")
-	for item in commit:
-		file.write(str(datetime.now()))
-		file.write(" %s\n"%item)
-	file.close()
+	try:
+		file=open("log.txt","r")
+		for item in commit:
+			file.write(str(datetime.now()))
+			file.write(" %s\n"%item)
+	except IOError:
+		print('Error can\'t write to file')
+	finally:
+		file.close()
 	g2=GiftSelector2(couplesList)
 	g2.assign()
 	g2.writ()
