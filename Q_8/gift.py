d91eecd8e86c47f37eef5508232068b66551152f
Abhishek Kumar <iit2015077@iiita.ac.in>
2017-04-14 00:01:23 +0530

    Update gift.py

--- Q_8/gift.py
+++ Q_8/gift.py
@@ -2,21 +2,22 @@ import random
 
 class Gifts:
 
-	util={'A':100,'B':75,'C':50,'D':20} #utility class as key while utility value as value
+	util={'A':100,'B':75,'C':50,'D':20}
 
 	def __init__(self,name,price,value,category):
 		self.name=name
 		self.price=price
 		self.value=value
-		self.category=category
 		
-		if category=='Luxury':
-			self.difficulty=random.randint(100)
+		try:
+			if category=='Luxury':
+				self.difficulty=random.randint(100)
 		
-		if category=='Utility':
-			u=util.popitem()
-			self.utilClass=u[0]
-			self.utilVal=u[1]
+			if category=='Utility':
+				u=util.popitem()
+				self.utilClass=u[0]
+				self.utilVal=u[1];
 
-	
-
+		except SyntaxError:
+			print('take care of syntax')
+		
