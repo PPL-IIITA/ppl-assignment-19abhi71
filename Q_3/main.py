e0e04ab8b3bcff1c09ca03359cee8b0d0952c203
Abhishek Kumar <iit2015077@iiita.ac.in>
2017-04-14 00:27:42 +0530

    Update main.py

--- Q_3/main.py
+++ Q_3/main.py
@@ -1,5 +1,12 @@
 from allIn import Boy,Girl,Gifts,PeopleAndObjects
-from couples import Couple
+try:
+	from boy import Boy
+	from girl import Girl
+	from gift import Gifts
+	from couples import Couple
+	from magic import awesome
+except ImportError:
+	print('There is error in importing some files')
 from datetime import datetime
 import operator
 import random
@@ -24,10 +31,13 @@ giftsList=[]
 #testing utility
 
 #generating a list named B of boy objects and boyslist with respective values of object for writing to csv
-for i in range(19):
-	b=Boy(boys[i],random.randint(10,100),random.randint(24,100),random.randint(50,5000),random.randint(6,50),random.choice(typ1))
-	boyslist.append([b.name,b.attractiveness,b.intelligence,b.budget,b.minattreq,b.typ,b.status])
-	B.append(b)
+try:
+	for i in range(22):
+		b=Boy(boys[i],random.randint(10,100),random.randint(24,100),random.randint(50,5000),random.randint(6,50),random.choice(typ1))
+		boyslist.append([b.name,b.attractiveness,b.intelligence,b.budget,b.minattreq,b.typ,b.status])
+		B.append(b)
+except IndexError:
+	print('List out of bounds')
 
 #writing csv file
 with open("guys.csv","w") as h:
@@ -35,28 +45,39 @@ with open("guys.csv","w") as h:
 	writer.writerows(boyslist)
 
 #generating a list named G of girl objects and girlslist with respective values of object for writing to csv
-for i in range(13):
-	g=Girl(girls[i],random.randint(10,100),random.randint(26,100),random.randint(12,5000),random.choice(crit),random.choice(typ2))
-	girlslist.append([g.name,g.attractiveness,g.intelligence,g.maintbudget,g.criteria,g.typ,g.status])
-	G.append(g)
+try:
+	for i in range(13):
+		g=Girl(girls[i],random.randint(10,100),random.randint(26,100),random.randint(12,5000),random.choice(crit),random.choice(typ2))
+		girlslist.append([g.name,g.attractiveness,g.intelligence,g.maintbudget,g.criteria,g.typ,g.status])
+		G.append(g)
+except StopIteration:
+	print('List out of bounds')
 
 #writing csv file
 with open("girls.csv","w") as f:
 	writer=csv.writer(f,delimiter=',')
 	writer.writerows(girlslist)
 
-for boy in B:
-	for girl in G:
-		#if boy and girls are both ready to pair with each other and are already not commited
-		if boy.readytopair(girl) and girl.readytopair(boy) and boy.currStatus()=='S' and girl.currStatus()=='S':
-			#change their status
-			boy.changeStatus()
-			girl.changeStatus()
-			c=Couple(boy.name,boy.typ,girl.typ,girl.name,boy.budget,girl.maintbudget,boy.attractiveness,girl.attractiveness,boy.intelligence,girl.intelligence)
-			couplesList.append(c)
-			s1=boy.name+' is gonna date '+girl.name
-			commit.append(s1)
-			break
+def findDates():
+	try:
+		for boy in B:
+			for girl in G:
+				#if boy and girls are both ready to pair with each other and are already not commited
+				if boy.readytopair(girl) and girl.readytopair(boy) and boy.currStatus()=='S' and girl.currStatus()=='S':
+					#change their status
+					boy.changeStatus()
+					girl.changeStatus()
+						
+					c=Couple(boy.name,boy.typ,girl.typ,girl.name,boy.budget,girl.maintbudget,boy.attractiveness,girl.attractiveness,boy.intelligence,girl.intelligence)
+					couplesList.append(c)
+					s1=boy.name+' is gonna date '+girl.name
+					commit.append(s1)
+					break
+	except IndentationError:
+		print('unexpected indentations')
+
+
+findDates()
 
 
 s1='Here starts the Valentine week of February:--------------------------------------------------------------'
@@ -90,7 +111,10 @@ for c in couplesList:
 		before-=item.price
 		c.priceTag.append(item.price)
 		c.valueTag.append(item.value)
-		spent+=item.price
+		try:
+			spent+=item.price/0
+		except ArithmeticError:
+			print('there is some error in arithmetics for this variable')
 		s1=c.bf+' gifts '+item.name+' '+c.gf
 		commit.append(s1)
 	
@@ -109,10 +133,15 @@ for c in couplesList:
 
 
 #taking note of all commitments and gift exchanges with time stamps
-file=open("log.txt","w")
-for item in commit:
-	file.write(str(datetime.now()))
-	file.write(" %s\n"%item)
+try:
+	file=open("log.txt","r")
+	for item in commit:
+		file.write(str(datetime.now()))
+		file.write(" %s\n"%item)
+except IOError:
+	print('Error can\'t write to file')
+finally:
+	file.close()
 
 #print k most happiest couples
 fate.sort(key=lambda x:x[0],reverse=True)
@@ -123,7 +152,11 @@ print(s1)
 hi=[x[0] for x in fate]
 topk=hi[:k]
 for item in topk:
-	s1=list(happyCouples.keys())[list(happyCouples.values()).index(item)] + ' with happiness value: '+str(item)
+	try:
+		s1=list(happyCouples.keys())[list(happyCouples.values()).index(item)] + ' with happiness value: '+str(item)
+	except TypeError:
+		print('can\'t convert int object to string implicitly')
+	
 	commit.append(s1)
 	print(s1)	
 print('\n')
@@ -137,7 +170,11 @@ print(s1)
 hi=[x[1] for x in fate]
 topk=hi[:k]
 for item in topk:
-	s1=list(compatibleCouples.keys())[list(compatibleCouples.values()).index(item)]+ ' with compatibility value: '+str(item)
+	try:
+		s1=list(compatibleCouples.keys())[list(compatibleCouples.values()).index(item)]+ ' with compatibility value: '+str(item)
+	except TypeError:
+		print('can\'t convert int object to string implicitly')
+	
 	commit.append(s1)
 	print(s1)
 
